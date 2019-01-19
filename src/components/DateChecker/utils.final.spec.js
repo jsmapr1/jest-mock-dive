3b32bf6cb54da1a22092ac81009017fc89e41d46
Joe Morgan <jsmapr1@gmail.com>
2019-01-19 17:33:16 -0500

    update

--- src/components/DateChecker/utils.final.spec.js
+++ src/components/DateChecker/utils.final.spec.js
@@ -2,20 +2,26 @@ import { isAtLeastTwoWeeks, message } from './utils';
 import moment from 'moment';
 jest.mock('moment');
 
-describe('iso weekend', () => {
-  it.skip('should return a two week message', () => {
-    moment.mockImplementation(() => ({
-      add: () => {},
-      isAfter: () => false
-    }));
+moment.mockImplementation(() => ({
+  add: () => {},
+  isAfter: () => false
+}));
+
+describe.skip('iso weekend', () => {
+  it('should return a two week message', () => {
     expect(isAtLeastTwoWeeks('2018-02-02')).toEqual(message);
   })
 
-  it.skip('should return no message', () => {
-    moment.mockImplementation(() => ({
+  it('should return no message', () => {
+    moment.mockImplementationOnce(() => ({
       add: () => {},
       isAfter: () => true
     }));
     expect(isAtLeastTwoWeeks('2018-01-14')).toBeFalsy();
   })
+
+  it('should return a two week message', () => {
+    expect(isAtLeastTwoWeeks('2018-02-02')).toEqual(message);
+  })
+
 })
