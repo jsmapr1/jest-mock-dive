481f8ba0eb4ce0fcdbadccadaed11fa68787c895
Joe Morgan <jsmapr1@gmail.com>
2019-07-22 10:07:43 -0400

    Add ridefinder

---
+++ src/components/DateChecker/utils.moment.spec.js
@@ -0,0 +1,11 @@
+import { isAtLeastTwoWeeks, message } from './utils';
+
+describe('iso weekend', () => {
+  it('should return a two week message', () => {
+    expect(isAtLeastTwoWeeks('2019-01-02')).toEqual(message);
+  })
+
+  it('should return no message', () => {
+    expect(isAtLeastTwoWeeks('2019-02-12')).toEqual('');
+  })
+})
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import moment from 'moment';
 
 configure({ adapter: new Adapter() });
 
 // beforeAll(() => {
-//   moment.tz.setDefault('America/New_York');
 //   moment.now = () => +new Date('2019-01-09T00:00:00.000Z');
 // });
